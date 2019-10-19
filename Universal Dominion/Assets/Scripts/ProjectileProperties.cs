009712ea94a50fc516bcfddf51b3ba81fe1b2f5f
Randal McSwiggins <54963165+RyzEofCHAOS@users.noreply.github.com>
2019-10-19 15:18:39 -0700

    Unity Files (Universal Dominion)
    Includes all asset files and scripts used.

---
+++ Universal Dominion/Assets/Scripts/ProjectileProperties.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileProperties : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float timer = 2f;
+
+    void Update()
+    {
+        Vector3 posy = transform.position;
+        Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+        posy += transform.rotation * velocity;
+
+        transform.position = posy;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
