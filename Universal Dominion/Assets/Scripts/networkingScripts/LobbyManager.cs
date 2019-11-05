d4a14852e93865f156d9f7949bd995d2066278dc
Hannselthill Camacho <45092062+Hannsel101@users.noreply.github.com>
2019-11-04 18:21:29 -0800

    Slight restructuring of lobby hosting GUI

---
+++ Universal Dominion/Assets/Scripts/networkingScripts/LobbyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class LobbyManager : NetworkLobbyManager
+{
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        print("A game was created!");
+    }
+
+}
---
+++ Universal Dominion/Assets/Scripts/networkingScripts/LobbyMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class LobbyMenu : MonoBehaviour
+{
+
+    public TMP_InputField LobbyNameInput;
+    public LobbyManager lobbyManager;
+
+    public void OnClickHostLobbyButton()
+    {
+        lobbyManager.StartMatchMaker();
+        lobbyManager.matchMaker.CreateMatch(LobbyNameInput.text, (uint)lobbyManager.maxPlayers, true, "", "", "", 0, 0, lobbyManager.OnMatchCreate);
+    }
+
+}
