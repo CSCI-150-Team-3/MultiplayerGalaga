97e7a3fa505f8e817d33d472051e8e6e82630603
Josh <joshualaofrancisco@hotmail.com>
2019-11-12 01:01:10 -0800

    added MySQL database
    added simple test function getdate, getusers, linked game to web browser mysql database

---
+++ Universal Dominion/Assets/Scripts/databaseScripts/Web.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetDate());
+        StartCoroutine(GetUsers());
+    }
+
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackend/Getdate.php"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackend/GetUsers.php"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+}
