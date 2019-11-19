4da230669bd15d1a5e729a8b947914cdce75d3ca
Randal McSwiggins <54963165+RyzEofCHAOS@users.noreply.github.com>
2019-11-18 18:42:09 -0800

    Wave 1 Complete

---
+++ Universal Dominion/Assets/Scripts/Wave1Scripts/PostWave1Complete.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWave1Complete : MonoBehaviour
+{
+    float delayCounter = 13f;
+    public GameObject wave1Prefab;
+    GameObject seekFinalShip;
+
+    void Update()
+    {
+        seekFinalShip = GameObject.Find("FrontEnemy");
+        if (seekFinalShip == null)
+        {
+            seekFinalShip = GameObject.Find("2ndLineEnemy");
+            if (seekFinalShip == null)
+            {
+                seekFinalShip = GameObject.Find("3rdLineEnemy");
+                if (seekFinalShip == null)
+                {
+                    seekFinalShip = GameObject.Find("RearEnemy");
+                    if (seekFinalShip == null)
+                    {
+                        delayCounter -= Time.deltaTime;
+                        if (delayCounter <= 0)
+                        {
+                            Destroy(wave1Prefab);
+                        }
+                    }
+                }
+            }        
+        }
+    }
+}
---
+++ Universal Dominion/Assets/Scripts/Wave1Scripts/Wave1_TitleScripts.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave1_TitleScripts : MonoBehaviour
+{
+    float delaycounter = 0.25f;
+    public GameObject readyPrefab;
+    public GameObject wavePrefab;
+    public GameObject incomingPrefab;
+    GameObject readySprite;
+    GameObject waveSprite;
+    GameObject incomingSprite;
+    GameObject waveOneEnd;
+    bool Ready = true;
+    bool Wave = false;
+    bool Wave1 = true;
+    bool postWave1 = false;
+    
+     void Update()
+    {
+        delaycounter -= Time.deltaTime;
+
+        if (delaycounter <= 0 && Ready && !Wave)
+        {
+            waveSprite = (GameObject)Instantiate(wavePrefab, transform.position, Quaternion.identity);
+            delaycounter = 2;
+            Ready = false;
+        }
+
+        if (delaycounter <= 0 && !Ready && !Wave)
+        {
+            waveSprite.SetActive(false);
+            readySprite = (GameObject)Instantiate(readyPrefab, transform.position, Quaternion.identity);
+            delaycounter = 2;
+            Wave = true;
+        }
+
+        waveOneEnd = GameObject.Find("Wave1");
+        if (delaycounter <= 0 && Wave && waveOneEnd != null)
+        {
+            readySprite.SetActive(false);
+        }
+
+        if (waveOneEnd == null && Wave1)
+        {
+            incomingSprite = (GameObject)Instantiate(incomingPrefab, transform.position, Quaternion.identity);
+            delaycounter = 2;
+            Wave1 = false;
+        }
+
+        if(delaycounter <= 0 && !Wave1 && !postWave1)
+        {
+            incomingSprite.SetActive(false);
+            readySprite.SetActive(true);
+            delaycounter = 2;
+            postWave1 = true;
+        }
+
+        if (delaycounter <= 0 && postWave1)
+        {
+            readySprite.SetActive(false);
+        }
+    }
+
+}
