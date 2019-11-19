42c9a9a4c931762d8d9a66be24cd074b5dee3f9a
Hannsel101 <hannselthillcamacho@gmail.com>
2019-11-19 14:26:47 -0800

    manually merged the database updates into development

---
+++ Universal Dominion/Assets/Scripts/databaseScripts/Web.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    void Start()
+    {
+        //StartCoroutine(GetDate());
+        //StartCoroutine(GetUsers());
+        //StartCoroutine(Login("testuser2", "qwert"));
+        //StartCoroutine(RegisterUser("testuser3", "12345" ));
+    }
+
+    //Access GetDate.php file in xampp -> htdocs -> UnityBackend
+    IEnumerator GetDate()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackend/Getdate.php"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    //Access GetUsers.php file in xampp -> htdocs -> UnityBackend
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackend/GetUsers.php"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    //Access Login.php file in xampp -> htdocs -> UnityBackend
+    public IEnumerator Login(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/UnityBackend/Login.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+    //Access RegisterUser.php file in xampp -> htdocs -> UnityBackend
+    IEnumerator RegisterUser(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost/UnityBackend/RegisterUser.php", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
