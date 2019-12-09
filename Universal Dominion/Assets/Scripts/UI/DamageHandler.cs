d2ab59b79b469bb0383e949756a1d82c42a080be
Rory <ryh835450485@gmail.com>
2019-12-08 20:57:38 -0800

    UI fixing with 2018 4.12 version

---
+++ Universal Dominion/Assets/Scripts/UI/DamageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+
+    [SerializeField] private Hullbar hullbar;
+    public int health = 3;
+    public float totalhealth = 3f;
+    public float normalizedhealth = 1f;
+    public float invulnerabilityPeriod;
+    float invulnerableTimer = 0;
+    int correctLayer;
+
+    void Start()
+    {
+        correctLayer = gameObject.layer;
+    }
+
+    void OnTriggerEnter2D()
+    {
+        health--;
+        normalizedhealth = health / totalhealth;
+        invulnerableTimer = invulnerabilityPeriod;
+        gameObject.layer = 10;
+    }
+    void Update()
+    {
+        invulnerableTimer -= Time.deltaTime;
+        if (invulnerableTimer <= 0)
+        {
+            gameObject.layer = correctLayer;
+        }
+        {
+            if(health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
